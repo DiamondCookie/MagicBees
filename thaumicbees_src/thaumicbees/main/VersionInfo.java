47e00474d0f85d89c09362f692192b7cc7a19a8e
MysteriousAges <mysteriousages@gmail.com>
2012-12-29 21:28:32 -0800

    Roflkopter, it's Logo time!

--- thaumicbees_src/thaumicbees/main/VersionInfo.java
+++ thaumicbees_src/thaumicbees/main/VersionInfo.java
@@ -7,6 +7,8 @@ public class VersionInfo
 	public static final String MCVersion = "@MCVERSION@";
 	public static final String Build = "@BUILD_NUMBER@";
 	
+	public static final String Logo = "/gfx/thaumicbees/logo.png";
+	
 	public static final String Depends = "required-after:Forestry@[1.6.4.2,);required-after:Thaumcraft@[3.0.1c,)";
 
 }
